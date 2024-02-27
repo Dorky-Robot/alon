610e3cb884682448aeec5bb29b534ce7c4476ddf
Felix Flores <felixflores@gmail.com>
2024-02-27 17:12:04 +0000

    🎖️: Implement receptor based subscription for alon

--- src/alon.js
+++ src/alon.js
@@ -1,225 +1,55 @@
 (function (window) {
-  const Stuff = {};
   const ALON_EVENT = '__AlonEvent__';
 
-  function signal({ element, path, payload }) {
-    const mergedDetail = { payload, timestamp: new Date().getTime(), path };
+  function get(path, object, defaultValue) {
+    const keys = path.replace(/\[(\d+)\]/g, '.$1').split('.');
+    let result = object;
 
+    for (let key of keys) {
+      if (result === undefined || result === null) {
+        return defaultValue;
+      }
+      // Convert key to a number if it's a valid array index
+      const index = isNaN(Number(key)) ? key : Number(key);
+      result = result.hasOwnProperty(index) ? result[index] : undefined;
+    }
+
+    return result !== undefined ? result : defaultValue;
+  }
+
+  function signal(element, payload) {
     element.dispatchEvent(new CustomEvent(this.ALON_EVENT, {
-      detail: mergedDetail,
+      detail: payload,
       bubbles: true,
     }));
   }
 
-  /**
-   * Internal function to register a handler based on a series of path segments.
-   * This function constructs or traverses a nested structure of handlers, registering
-   * the provided handler function at the level indicated by the pathSegments array.
-   * It's designed for internal use within a larger routing or event handling system
-   * where paths need to be dynamically registered and associated with specific handlers.
-   * 
-   * Note: This function mutates the `handlers` object in place for efficiency.
-   * 
-   */
-  function _registerSubscriber({ pathSegments, handlers, handler }) {
-    const [segment, ...rest] = pathSegments;
+  function subscribe(element, resolver, handler) {
+    if (!element.alonHandlers) {
+      element.alonHandlers = new Map();
 
-    if (rest.length > 0) {
-      handlers[segment] = handlers[segment] || {};
-
-      _registerSubscriber({
-        pathSegments: rest, handlers: handlers[segment], handler
+      element.addEventListener(this.ALON_EVENT, (e) => {
+        for (const [resolver, handlers] of e.currentTarget.alonHandlers.entries()) {
+          const result = resolver(e.detail);
+          // Check if result is not undefined to call handlers
+          if (result !== undefined) {
+            handlers.forEach((handler) => handler(result, e));
+          }
+        }
       });
+    }
+
+    if (element.alonHandlers.has(resolver)) {
+      element.alonHandlers.get(resolver).push(handler);
     } else {
-      if (segment === '*') {
-        handlers[segment] = handlers[segment] || [];
-        handlers[segment].push(handler);
-      } else {
-        handlers[segment] = handlers[segment] || {};
-        handlers[segment]['*'] = handlers[segment]['*'] || []
-        handlers[segment]['*'].push(handler);
-      }
+      element.alonHandlers.set(resolver, [handler]);
     }
-  };
-
-  function _getHandlers({ pathSegments, candidates }) {
-    const [segment, ...rest] = pathSegments;
-
-    let handlers = [];
-
-    if (candidates[segment] && candidates[segment]['*']) {
-      handlers.push(...candidates[segment]['*']);
-    }
-
-    if (rest.length > 0 && candidates[segment]) {
-      handlers.push(..._getHandlers({
-        pathSegments: rest,
-        candidates: candidates[segment]
-      }));
-    }
-
-    return handlers;
   }
 
-  function subscribe({ element, path, handler }) {
-    element.addEventListener(this.ALON_EVENT, (e) => {
-      const handlers = _getHandlers({
-        pathSegments: e.detail.path.split('.'),
-        candidates: e.currentTarget.alonHandlers
-      });
-
-      handlers.map(f => f(e.detail.payload))
-    });
-
-    element.alonHandlers = element.alonHandlers || {}; // Add data property to elem object
-    _registerSubscriber({
-      pathSegments: path.split('.'),
-      handlers: element.alonHandlers,
-      handler
-    });
-
-  }
 
   window.Alon = {
+    get,
     signal,
-    subscribe,
-    _registerSubscriber
+    subscribe
   };
 })(window);
-
-
-// Alon.dispatch = function ({ path, payload }) {
-//   Alon.listeners = Alon.listeners || {};
-//   if (Object.values(Alon.listeners).length === 0) return;
-
-//   let handlers = [];
-//   const dispatchTo = (segments, obj) => {
-//     // If there are no more segments or obj is undefined, stop the recursion
-//     if (!segments.length) return;
-//     const segment = segments.shift();
-
-//     if (Array.isArray(obj[segment])) {
-//       if (segment !== '*') handlers.push(...obj['*']);
-//       handlers.push(...obj[segment]);
-//     } else {
-//       dispatchTo(segments, obj[segment]);
-//     }
-//   };
-
-//   console.log(path, handlers)
-//   dispatchTo(path.split('.'), Alon.listeners);
-//   handlers.map((handler) => handler(payload));
-// }
-
-
-
-// Alon.init = function () {
-//   document.addEventListener(this.ALON_EVENT, (e) => {
-//     Alon.dispatch({
-//       path: e.detail.path,
-//       payload: e.detail.payload
-//     })
-//   });
-// }
-// document.addEventListener('DOMContentLoaded', () => {
-//   //   Alon.init();
-
-//   //   Alon.subscribe(document, 'person.first', (e) => {
-//   //     console.log('person.first', e)
-//   //   })
-
-//   Alon.subscribe(document, 'person.*', (e) => {
-//     console.log('person.*', e)
-//   })
-
-//   //   Alon.subscribe(document, 'person.name', (e) => {
-//   //     console.log('person.name', e)
-//   //   })
-// });
-
-
-
-// function Text({ name, placeholder }) {
-//   return ['input', { type: 'text', name, placeholder }];
-// }
-
-// function TextArea({ name, placeholder }) {
-//   return ['textarea', { name, placeholder }];
-// }
-
-// function RadioGroup(name, choices) {
-//   const choiceElements = choices.flatMap((choice) => {
-//     const id = `${name}-${choice.value.replace(/\s+/g, '-')}`;
-//     return [
-//       ['input', { type: 'radio', name: name, id: id, value: choice.value }],
-//       ['label', { for: id }, choice.label]
-//     ];
-//   });
-
-//   return ['fieldset', ...choiceElements];
-// }
-
-// customElements.define('alon-input',
-//   class extends HTMLElement {
-//     constructor() {
-//       super();
-
-//       const form = Habiscript.toElement([
-//         'form',
-//         TextArea({ name: 'input', placeholder: 'Alon Message' }),
-//         ['button', 'Send']
-//       ]);
-
-//       const shadowRoot = this.attachShadow({ mode: 'open' });
-//       shadowRoot.appendChild(form);
-//     }
-
-//     get input() {
-//       return this.shadowRoot.querySelector('[name=input]');
-//     }
-
-//     get form() {
-//       return this.shadowRoot.querySelector('form');
-//     }
-
-//     get value() {
-//       return this.input.value;
-//     }
-
-//     connectedCallback() {
-//       const self = this;
-
-//       this.form.addEventListener('submit', (e) => {
-//         e.preventDefault();
-
-//         Alon.signal({
-//           element: self,
-//           path: 'person',
-//           payload: { name: self.value }
-//         });
-
-//         self.form.reset();
-//       });
-//     }
-//   }
-// )
-
-
-// const button = Habiscript.toElement([
-//   'button',
-//   {
-//     onclick: (e) => {
-//       e.preventDefault();
-//       document.dispatchEvent(new CustomEvent('alon.person', {
-//         bubbles: true, cancelable: true,
-//         detail: {
-//           value: 'testing'
-//         }
-//       }));
-//     }
-//   },
-//   'Click me'
-// ])
-
-// document.body.appendChild(button);
-
--- src/alon.spec.js
+++ src/alon.spec.js
@@ -1,3 +1,91 @@
+describe("get", () => {
+  it("returns the handlers that exactly matches the path", () => {
+    const val = Alon.get('person.name', { person: { name: 2 } })
+    expect(val).toEqual(2);
+  });
+
+  it("accessing a nested property using an array path", () => {
+    const val = Alon.get(
+      'user.address.city',
+      {
+        user: {
+          address: {
+            city: "Lorain",
+            state: "Ohio"
+          }
+        }
+      }
+    );
+
+    expect(val).toEqual("Lorain");
+  });
+
+  it("accessing a specific elements of an array", () => {
+    const val = Alon.get(
+      'user.addresses[1].city',
+      {
+        user: {
+          addresses: [ // Corrected from 'address' to 'addresses'
+            { city: "Lorain" },
+            { city: "Cleveland" }
+          ]
+        }
+      }
+    );
+
+    expect(val).toEqual("Cleveland");
+  });
+
+  it("returns undefined when accessing index that is out of bound", () => {
+    const val = Alon.get(
+      'user.addresses[33].city',
+      {
+        user: {
+          addresses: [ // Corrected from 'address' to 'addresses'
+            { city: "Lorain" },
+            { city: "Cleveland" }
+          ]
+        }
+      }
+    );
+
+    expect(val).toEqual(undefined);
+  });
+
+  it("attempting to access a non-existent property", () => {
+    const val = Alon.get(
+      'user.city',
+      {
+        user: {
+          addresses: [ // Corrected from 'address' to 'addresses'
+            { city: "Lorain" },
+            { city: "Cleveland" }
+          ]
+        }
+      }
+    );
+
+    expect(val).toEqual(undefined);
+  });
+
+  it("attempting to access a non-existent property with default value", () => {
+    const val = Alon.get(
+      'user.city',
+      {
+        user: {
+          addresses: [ // Corrected from 'address' to 'addresses'
+            { city: "Lorain" },
+            { city: "Cleveland" }
+          ]
+        }
+      },
+      "Not found"
+    );
+
+    expect(val).toEqual("Not found");
+  });
+});
+
 describe("subscribe", () => {
   it("allows elements to subscribe to specific paths", () => {
     const span = Habiscript.toElement(['span', { id: 'name' }]);
@@ -7,88 +95,147 @@ describe("subscribe", () => {
 
     document.body.appendChild(container);
 
-    let num = 3;
-    function handler(payload) {
-      num = num + payload;
-    }
-
-    Alon.subscribe({ element: container, path: 'person.name', handler });
-    Alon.signal({ element: span, path: 'person.name', payload: 2 });
-
-    expect(num).toEqual(5);
-  });
-
-  it('allows elements to subscribe hierarchically', () => {
-    const span = Habiscript.toElement(['span', { id: 'name' }]);
-    const container = Habiscript.toElement(
-      ['div', { id: 'person' }, span]
+    let name;
+    Alon.subscribe(
+      container,
+      (p) => { return Alon.get('person.name', p) },
+      (r) => {
+        name = r;
+      }
     );
 
-    document.body.appendChild(container);
-
-    let num = 3;
-    function handler(payload) {
-      num = num + payload;
-    }
-
-    Alon.subscribe({ element: container, path: 'person', handler });
-    Alon.signal({ element: span, path: 'person.name', payload: 2 });
-
-    expect(num).toEqual(5);
-  });
-
-
-  it('allows wildcard event subscription', () => {
-    const span = Habiscript.toElement(['span', { id: 'name' }]);
-    const container = Habiscript.toElement(
-      ['div', { id: 'person' }, span]
+    Alon.signal(
+      span,
+      { person: { name: 'Felix' } }
     );
 
+    expect(name).toEqual('Felix');
+  });
+
+  it("calls multiple handlers for different resolvers", () => {
+    const span = Habiscript.toElement(['span', { id: 'name' }]);
+    const container = Habiscript.toElement(['div', { id: 'person' }, span]);
     document.body.appendChild(container);
 
-    let num = 3;
-    function handler(payload) {
-      num = num + payload;
-    }
+    let firstName;
+    let lastName;
+    Alon.subscribe(
+      container,
+      (p) => Alon.get('person.firstName', p), // Resolver for the first name
+      (r) => { firstName = r; }
+    );
+    Alon.subscribe(
+      container,
+      (p) => Alon.get('person.lastName', p), // Resolver for the last name
+      (r) => { lastName = r; }
+    );
 
-    Alon.subscribe({ element: container, path: 'person.*', handler });
-    Alon.signal({ element: span, path: 'person.name', payload: 2 });
+    Alon.signal(span, { person: { firstName: 'Felix', lastName: 'Flores' } });
 
-    expect(num).toEqual(5);
-  });
-});
-
-
-describe('_registerSubscriber', () => {
-  it('adds a subscriber to the subscribers array found in "*"', () => {
-    const handlers = {};
-    const handler = () => { };
-    Alon._registerSubscriber({
-      pathSegments: ['person'],
-      handlers,
-      handler
-    });
-
-    expect(handlers['person']['*']).toEqual([handler]);
+    expect(firstName).toEqual('Felix');
+    expect(lastName).toEqual('Flores');
   });
 
-  it('appends new handlers instead of overrides', () => {
-    const handlers = {};
-    const handler1 = () => { };
-    const handler2 = () => { };
+  it("does not call handlers when the resolver returns undefined", () => {
+    const span = Habiscript.toElement(['span', { id: 'name' }]);
+    const container = Habiscript.toElement(['div', { id: 'person' }, span]);
+    document.body.appendChild(container);
 
-    Alon._registerSubscriber({
-      pathSegments: ['person'],
-      handlers,
-      handler: handler1
-    });
+    let nameCalled = false;
+    Alon.subscribe(
+      container,
+      (p) => undefined, // Resolver fails to resolve
+      (r) => { nameCalled = true; }
+    );
 
-    Alon._registerSubscriber({
-      pathSegments: ['person'],
-      handlers,
-      handler: handler2
-    });
+    Alon.signal(span, { person: { name: 'Felix' } });
 
-    expect(handlers['person']['*']).toEqual([handler1, handler2]);
+    expect(nameCalled).toBe(false);
+  });
+
+  it("does call handlers when the resolver returns false", () => {
+    const span = Habiscript.toElement(['span', { id: 'name' }]);
+    const container = Habiscript.toElement(['div', { id: 'person' }, span]);
+    document.body.appendChild(container);
+
+    let nameCalled = false;
+    Alon.subscribe(
+      container,
+      (p) => false, // Resolver returns false
+      (r) => { nameCalled = true; }
+    );
+
+    Alon.signal(span, { person: { name: 'Felix' } });
+
+    expect(nameCalled).toBe(true);
+  });
+
+  it("calls the same resolver only once but multiple handlers for that resolver", () => {
+    const span = Habiscript.toElement(['span', { id: 'name' }]);
+    const container = Habiscript.toElement(['div', { id: 'person' }, span]);
+    document.body.appendChild(container);
+
+    let resolverCallCount = 0;
+    const resolver = (p) => {
+      resolverCallCount++;
+      return Alon.get('person.name', p);
+    };
+
+    let name1;
+    let name2;
+    Alon.subscribe(
+      container,
+      resolver,
+      (r) => { name1 = r; }
+    );
+    Alon.subscribe(
+      container,
+      resolver,
+      (r) => { name2 = r; }
+    );
+
+    Alon.signal(span, { person: { name: 'Felix Flores' } });
+
+    // Check that the resolver was called only once
+    expect(resolverCallCount).toEqual(1);
+
+    // Check that both handlers were called with the resolved value
+    expect(name1).toEqual('Felix Flores');
+    expect(name2).toEqual('Felix Flores');
+  });
+
+  it("does not propagate the event to the outer element when stopPropagation is called", () => {
+    const span = Habiscript.toElement(['span', { id: 'name' }]);
+    const inner = Habiscript.toElement(['div', { id: 'person' }, span]);
+    const outer = Habiscript.toElement(['div', inner]);
+
+    document.body.appendChild(outer);
+
+    let innerHandlerCalled = false;
+    let outerHandlerCalled = false;
+
+    // Subscribe handler for the inner element and stop propagation
+    Alon.subscribe(
+      inner,
+      (p) => Alon.get('person.name', p),
+      (r, e) => {
+        innerHandlerCalled = true;
+        e.stopPropagation(); // Stop the event from bubbling up
+      }
+    );
+
+    // Subscribe handler for the outer element
+    Alon.subscribe(
+      outer,
+      (p) => Alon.get('person.name', p),
+      (r, e) => {
+        outerHandlerCalled = true;
+      }
+    );
+
+    Alon.signal(span, { person: { name: 'Felix Flores' } });
+
+    expect(innerHandlerCalled).toBe(true);
+    expect(outerHandlerCalled).toBe(false);
   });
 })
